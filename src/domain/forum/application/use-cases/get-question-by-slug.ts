40ed5e16e12b5f9eae57fc95a1c05ffa68e23154
MarlonChi <marlonchiodelli@hotmail.com>
2025-01-16 22:16:46 -0300

    feat: read-notification use case and change import error

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -2,7 +2,7 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Either, left, right } from "@/core/either";
-import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string;
